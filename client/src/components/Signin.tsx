c960c91f4080c84759618b5c64d1bed92ac6581c
anjalikray <anjalikumari2907@gmail.com>
2024-09-29 23:49:16 +0530

    signin ui

--- client/src/components/Signin.tsx
+++ client/src/components/Signin.tsx
@@ -4,7 +4,7 @@ const Signin = () => {
     return (
         <div className="h-screen w-screen bg-circle-login">
             <div className=" flex  gap-5 justify-center  border-2 border-red-500 h-screen w-screen">
-                <div className="w-full">
+                <div className="w-ful ">
                     <img src="logo.png" alt="logo" className="mt-10 ml-10" />
                     <h1 className="text-center m-2 text-3xl font-semibold">
                         Sign In
@@ -33,11 +33,11 @@ const Signin = () => {
 
                 </div>
 
-                <div className="justify-items-center content-center">
+                <div className="flex justify-center items-center h-screen">
                     <img
                         src="signin_pic.png"
                         alt="food"
-                        className="relative w-3/5 "
+                        className="relative w-3/6 "
                     />
                 </div>
             </div>
