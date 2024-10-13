50f6b7069f7d54f334a42ec8d2da1aa8b84e2389
anjalikray <anjalikumari2907@gmail.com>
2024-10-13 18:56:47 +0530

    implemented shadcn on singin page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,13 +2,11 @@ import {Routes , Route} from 'react-router-dom'
 import Dashboard from './Pages/dashboard/Dashboard'
 import Signin from './Pages/auth/Signin'
 import Signup from './Pages/auth/Signup'
-import Header from './Pages/Header'
 import ForgotPassword from './Pages/auth/ForgotPassword'
 
 function App() {
     return (
         <>
-            <Header />
             <Routes>
                 <Route path="/signin" element={<Signin />} />
                 <Route path="/signup" element={<Signup />} />
--- client/src/Components/ui/button.tsx
+++ client/src/Components/ui/button.tsx
@@ -10,14 +10,14 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default:
-          "bg-primary text-primary-foreground shadow hover:bg-primary/90",
+          "bg-orange-500 self-center hover:bg-orange-700 my-5 w-fit justify-center items-center p-2.5 text-white text-md rounded",
         destructive:
           "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
         outline:
           "border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground",
         secondary:
           "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
-        ghost: "hover:bg-accent hover:text-accent-foreground",
+        ghost: "text-lg text-orange-500 font-normal",
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
