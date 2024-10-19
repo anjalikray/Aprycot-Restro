ae6d76a00a8d874de6b19d51ef4377784695b83a
anjalikray <anjalikumari2907@gmail.com>
2024-10-19 23:36:19 +0530

    fix: navigate to user dashboard

--- client/src/context/authContext.tsx
+++ client/src/context/authContext.tsx
@@ -47,7 +47,7 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
             // Cookies.set('refreshToken', data.refresh, { expires: 7 }); // 7 days expiry
             // Cookies.set('accessToken', data.access, { expires: data.expired_in_hours / 24 });
             navigate("/user-dashboard");
-            setUser(() => ({ email: data.email, name: data.name, role: data.role }));
+            setUser(() => ({ email: data.email, name: data.name }));
             setIsAuthenticated(true);
         } catch (error: any) {
             // toast.error("Couldn't signin with the provided credentials", { id: "signin" });
