7f053da83b3d8e99895ea2f3620b4a1a496f8475
anjalikray <anjalikumari2907@gmail.com>
2024-11-18 21:54:24 +0530

    cors config changes

--- server/src/index.ts
+++ server/src/index.ts
@@ -15,7 +15,6 @@ app.use(cors(corsOption));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true }));
 
-// app.use(cors({origin:"http://localhost:5174" , credentials: true}));
 
 app.use("/api", appRouter);
 
